6a96d32ed5778f1923e95360a0a7055a05612d10
Trent Lennon <tlennon@ebsco.com>
2016-11-17 09:16:57 -0500

    Moved the folder management into a method of its' own.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,118 +19,30 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Vector;
 
 public class Main extends Application {
-
+    private String pathWithoutUser = "C:\\Users\\%username%\\AppData\\LocalLow\\Ludeon Studios\\RimWorld";
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("RimWorld Save Manager");
 
-        String username = System.getProperty("user.name");
-        String path = "C:\\Users\\%username%\\AppData\\LocalLow\\Ludeon Studios\\RimWorld";
-        String pathToWindowsFolder = path.replace("%username%", username);
+        GridPane folderManagement = folderManagementGrid(primaryStage);
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25,25,25,25));
-        grid.setGridLinesVisible(true);
+        GridPane containerPane = new GridPane();
+        containerPane.setAlignment(Pos.TOP_CENTER);
+        containerPane.setHgap(10);
+        containerPane.setVgap(10);
+        containerPane.setPadding(new Insets(25,25,25,25));
+        containerPane.setGridLinesVisible(true);
+        containerPane.add(folderManagement,0,0);
 
-        Label saveFileLocationLabel = new Label("RimWorld Save Folder: ");
-        saveFileLocationLabel.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
-
-        Label managedSaveLabel = new Label("RWSM Folder: ");
-        managedSaveLabel.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
-
-        TextField saveFileLocationTxt = new TextField();
-        saveFileLocationTxt.setText(pathToWindowsFolder );
-        saveFileLocationTxt.setPrefWidth(800);
-        TextField rwsmLocationTxt = new TextField();
-        rwsmLocationTxt.setText(GetCurrentExecutionPath().toString());
-
-        Button saveLocationDir = new Button("...");
-        saveLocationDir.setTooltip(new Tooltip("Open the directory picker to find the save folder for RimWorld"));
-
-        Button managedSaveLocationDirPicker = new Button("...");
-        managedSaveLocationDirPicker.setTooltip(new Tooltip("Open the directory picker to find the location of your managed saves."));
-
-        saveLocationDir.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Opening the folder directory.");
-                DirectoryChooser directoryChooser = new DirectoryChooser();
-                directoryChooser.setInitialDirectory(new File(pathToWindowsFolder));
-
-                try {
-
-
-                    File selectedDirectory = directoryChooser.showDialog(primaryStage);
-                    saveFileLocationTxt.setText(selectedDirectory.getPath());
-                }
-                catch(IllegalArgumentException e) {
-                    System.out.println("Directory does not exist.");
-                    directoryChooser.setInitialDirectory(new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()));
-                    File selectedDirectory = directoryChooser.showDialog(primaryStage);
-                    saveFileLocationTxt.setText(selectedDirectory.getPath());
-
-                    //TODO: Set the status in the bottom with appropriate warning.
-                    //TODO: Look into creating a StatusBar to reflect what is happening with the program.
-                }
-            }
-        });
-        managedSaveLocationDirPicker.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Opening the folder directory.");
-
-                DirectoryChooser directoryChooser = new DirectoryChooser();
-                directoryChooser.setInitialDirectory(new File(GetCurrentExecutionPath().toString()));
-                directoryChooser.setTitle("RimWorld Save Manager Save Location");
-
-                try {
-                    File selectedDirectory = directoryChooser.showDialog(primaryStage);
-                    rwsmLocationTxt.setText(selectedDirectory.getPath());
-                }
-                catch(IllegalArgumentException e) {
-                    System.out.println("Directory does not exist.");
-                    directoryChooser.setInitialDirectory(new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()));
-                    File selectedDirectory = directoryChooser.showDialog(primaryStage);
-                    rwsmLocationTxt.setText(selectedDirectory.getPath());
-
-                    //TODO: Set the status in the bottom with appropriate warning.
-                    //TODO: Look into creating a StatusBar to reflect what is happening with the program.
-                }
-            }
-        });
-
-        File managedSaveFolder = SetupSaveManagerFolder();
-        List<File> gameSaves = WalkManagedSaveFolder(managedSaveFolder);
-
-        ListView managedSaves = new ListView();
-        ObservableList saves = FXCollections.observableArrayList();
-
-        for( File save : gameSaves ) {
-            saves.add(save.getName());
-        }
-
-        managedSaves.setItems(saves);
-
-        //RimWorld Save Stuff.
-        grid.add(saveFileLocationLabel,0,0, 1, 1);
-        grid.add(saveFileLocationTxt, 1,0, 4, 1);
-        grid.add(saveLocationDir,5,0,1,1);
-
-        //RimWorld Save Manager Stuff.
-        grid.add(managedSaveLabel,0,1);
-        grid.add(rwsmLocationTxt, 1, 1, 4, 1);
-        grid.add(managedSaveLocationDirPicker,5,1,1,1);
 
         //Managed Saves.
         //grid.add(managedSaves, 0, 2, 3, 2);
-        primaryStage.setScene(new Scene(grid, 800, 480));
+        primaryStage.setScene(new Scene(containerPane, 800, 480));
 
         primaryStage.show();
     }
@@ -159,6 +71,118 @@ public class Main extends Application {
         finally {
             return saveFolder;
         }
+    }
+
+    /**
+     * I hate doing this, but I hate the clutter of the start method even more.
+     * @param primaryStage
+     * @return
+     */
+    private GridPane folderManagementGrid(Stage primaryStage) {
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+        //grid.setGridLinesVisible(true);
+
+        Label saveFileLocationLabel = new Label("RimWorld Save Folder: ");
+        saveFileLocationLabel.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+
+        Label managedSaveLabel = new Label("RWSM Folder: ");
+        managedSaveLabel.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+
+        TextField saveFileLocationTxt = new TextField();
+        saveFileLocationTxt.setText(PathToWindowsFolder());
+        saveFileLocationTxt.setPrefWidth(800);
+        TextField rwsmLocationTxt = new TextField();
+        rwsmLocationTxt.setText(GetCurrentExecutionPath().toString());
+
+        Button saveLocationDir = new Button("...");
+        saveLocationDir.setTooltip(new Tooltip("Open the directory picker to find the save folder for RimWorld"));
+
+        Button managedSaveLocationDirPicker = new Button("...");
+        managedSaveLocationDirPicker.setTooltip(new Tooltip("Open the directory picker to find the location of your managed saves."));
+
+        saveLocationDir.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Opening the folder directory.");
+                DirectoryChooser directoryChooser = new DirectoryChooser();
+                directoryChooser.setInitialDirectory(new File(PathToWindowsFolder()));
+
+                try {
+
+
+                    File selectedDirectory = directoryChooser.showDialog(primaryStage);
+                    saveFileLocationTxt.setText(selectedDirectory.getPath());
+                }
+                catch(IllegalArgumentException e) {
+                    System.out.println("Directory does not exist.");
+                    directoryChooser.setInitialDirectory(new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()));
+                    File selectedDirectory = directoryChooser.showDialog(primaryStage);
+                    saveFileLocationTxt.setText(selectedDirectory.getPath());
+
+                    //TODO: Set the status in the bottom with appropriate warning.
+                    //TODO: Look into creating a StatusBar to reflect what is happening with the program.
+                }
+                catch(NullPointerException e) {
+                    //This typically means that cancel was hit when selecting a directory.
+                }
+            }
+        });
+        managedSaveLocationDirPicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Opening the folder directory.");
+
+                DirectoryChooser directoryChooser = new DirectoryChooser();
+                directoryChooser.setInitialDirectory(new File(GetCurrentExecutionPath().toString()));
+                directoryChooser.setTitle("RimWorld Save Manager Save Location");
+
+                try {
+                    File selectedDirectory = directoryChooser.showDialog(primaryStage);
+                    rwsmLocationTxt.setText(selectedDirectory.getPath());
+                }
+                catch(IllegalArgumentException e) {
+                    System.out.println("Directory does not exist.");
+                    directoryChooser.setInitialDirectory(new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()));
+                    File selectedDirectory = directoryChooser.showDialog(primaryStage);
+                    rwsmLocationTxt.setText(selectedDirectory.getPath());
+
+                    //TODO: Set the status in the bottom with appropriate warning.
+                    //TODO: Look into creating a StatusBar to reflect what is happening with the program.
+                }
+                catch(NullPointerException e) {
+                    //This typically means that cancel was hit when selecting a directory.
+                }
+            }
+        });
+
+        File managedSaveFolder = SetupSaveManagerFolder();
+        List<File> gameSaves = WalkManagedSaveFolder(managedSaveFolder);
+
+        ListView managedSaves = new ListView();
+        ObservableList saves = FXCollections.observableArrayList();
+
+        for( File save : gameSaves ) {
+            saves.add(save.getName());
+        }
+
+        managedSaves.setItems(saves);
+
+        //RimWorld Save Stuff.
+        grid.add(saveFileLocationLabel,0,0, 1, 1);
+        grid.add(saveFileLocationTxt, 1,0, 4, 1);
+        grid.add(saveLocationDir,5,0,1,1);
+
+        //RimWorld Save Manager Stuff.
+        grid.add(managedSaveLabel,0,1);
+        grid.add(rwsmLocationTxt, 1, 1, 4, 1);
+        grid.add(managedSaveLocationDirPicker,5,1,1,1);
+
+        return grid;
     }
 
     /**
@@ -193,6 +217,10 @@ public class Main extends Application {
         return new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
     }
 
+    private String PathToWindowsFolder() {
+        String username = System.getProperty("user.name");
+        return pathWithoutUser.replace("%username%", username);
+    }
     public static void main(String[] args) {
         launch(args);
     }
